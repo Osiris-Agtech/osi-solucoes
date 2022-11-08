33b778de9198a60dabb71c185254512fed556cef
juaodantas <jvdantasilva@hotmail.com>
2022-11-08 00:02:46 -0400

    chore: primeira tela estatica cadastro lote

--- lib/features/presenter/views/caderno_campo/components/lotePage.dart
+++ lib/features/presenter/views/caderno_campo/components/lotePage.dart
@@ -7,15 +7,13 @@ import 'package:osi_solucoes/features/presenter/viewmodels/caderno_campo_store.d
 Container lotePage(BuildContext context, CadernoCampoStore store) {
   return Container(
     height: MediaQuery.of(context).size.height * 0.9,
-    margin: EdgeInsets.only(
+    margin: const EdgeInsets.only(
       top: 0,
-      left: MediaQuery.of(context).size.width * 0.08,
-      right: MediaQuery.of(context).size.width * 0.08,
     ),
     child: Column(
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: 10),
+          padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
           child: RichText(
             textAlign: TextAlign.start,
             text: const TextSpan(
@@ -40,7 +38,46 @@ Container lotePage(BuildContext context, CadernoCampoStore store) {
             ),
           ),
         ),
-        const Spacer(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xffF5F5F5),
+              ),
+              child: ListView(
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.2,
+                        vertical: MediaQuery.of(context).size.height * 0.12,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Checkbox(value: false, onChanged: (_) {}),
+                              const Text('Nome cultura'),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ],
     ),
   );
