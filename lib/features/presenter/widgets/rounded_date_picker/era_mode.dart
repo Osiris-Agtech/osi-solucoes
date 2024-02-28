da6f4ff80dcf11446a62397ab31b983ef372fc67
Gabriel Moreira <g.tabordamoreira@gmail.com>
2024-02-27 23:14:43 -0400

    Adicionado date picker base

---
+++ lib/features/presenter/widgets/rounded_date_picker/era_mode.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
+enum EraMode { BUDDHIST_YEAR, CHRIST_YEAR }
+
+int calculateYearEra(EraMode? era, int christYear) {
+  if (era == EraMode.BUDDHIST_YEAR) {
+    return christYear + 543;
+  }
+  return christYear;
+}
---
+++ lib/features/presenter/widgets/rounded_date_picker/thai_date_utils.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: constant_identifier_names
+
+class ThaiDateUtils {
+  static const List<String> MONTH_FULL_TH = [
+    "มกราคม",
+    "กุมภาพันธ์",
+    "มีนาคม",
+    "เมษายน",
+    "พฤษภาคม",
+    "มิถุนายน",
+    "กรกฎาคม",
+    "สิงหาคม",
+    "กันยายน",
+    "ตุลาคม",
+    "พฤศจิกายน",
+    "ธันวาคม",
+  ];
+
+  static const List<String> MONTH_SHORT_TH = [
+    "ม.ค.",
+    "ก.พ.",
+    "มี.ค.",
+    "เม.ย.",
+    "พ.ค.",
+    "มิ.ย.",
+    "ก.ค.",
+    "ส.ค.",
+    "ก.ย.",
+    "ต.ค.",
+    "พ.ย.",
+    "ธ.ค.",
+  ];
+
+  static getMonthNameShot(int month) {
+    return MONTH_SHORT_TH[month - 1];
+  }
+
+  static getMonthNameFull(int month) {
+    return MONTH_FULL_TH[month - 1];
+  }
+}
